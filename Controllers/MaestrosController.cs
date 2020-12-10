655e15f079f937f2d4153c99232afcd9cffbe68c
jhosefgrey <jhosefgrey@hotmail.com>
2020-12-10 11:01:29 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/MaestrosController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mantenimiento.Models;
+using Mantenimiento.Models.ViewModels;
+
+namespace Mantenimiento.Controllers
+{
+    public class MaestrosController : Controller
+    {
+        // GET: Maestros
+        public ActionResult Index()
+        {
+            List<MaestroViewlModel> lista;
+            using (db_colegioEntities db = new db_colegioEntities())
+            {
+                lista = (from d in db.tb_maestros
+                         select new MaestroViewlModel
+                         {
+                             id = d.id,
+                             apellido = d.apellido,
+                             nombre = d.nombre
+                         }).ToList();
+            }
+
+            return View(lista);
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo(MaestrosViewModelA modelA)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (db_colegioEntities db= new db_colegioEntities())
+                    {
+                        var oMaestros = new tb_maestros();
+                        oMaestros.apellido = modelA.apellido;
+                        oMaestros.nombre = modelA.nombre;
+
+                        db.tb_maestros.Add(oMaestros);
+                        db.SaveChanges();
+                    }
+                }
+               return Redirect("Maestros/");
+            }
+            catch(Exception ex) 
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ Models/ViewModels/MaestrosViewModelA.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mantenimiento.Models.ViewModels
+{
+    public class MaestrosViewModelA
+    {
+            public int id { get; set; }
+            [Required]
+            [StringLength(30)]
+            [Display(Name = "apellido")]
+            public string apellido { get; set; }
+            [Required]
+            [StringLength(30)]
+            [Display(Name = "nombre")]
+            public string nombre { get; set; }
+        }
+    
+}
